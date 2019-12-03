c8cd6597ef8b1228cd392e09702604ed6fec9083
ehanbidge <ehanbidge@gmail.com>
2019-12-03 17:28:26 +0000

    animated test working

---
+++ typewriter.js
@@ -0,0 +1,14 @@
+// take a sentence and display it as an animation
+
+
+
+const sentence = "hello there from lighthouse labs";
+
+let i = 50;
+
+for (const char of sentence) {
+  setTimeout(() => process.stdout.write(char), i);
+  i += 50;
+  
+}
+setTimeout(() => process.stdout.write('\n'), i);
